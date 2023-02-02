43d0c3b1436b4d26db1e3fd2ef602ec57f46e6f2
Seunggyu <seunggyul14@icloud.com>
2023-02-02 10:58:19 -0800

    commit tree de20038ab89f89719368e862e72194a41d102611

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,35 @@
 class UsersController < ApplicationController
-
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
   end
+ 
+  def index
+    user = User.find(params[:user_id])
+    items = user.items
+    render json: items, include: :user
+  end
 
+  def item
+    user = User.find(params[:user_id])
+    items = user.items
+    item = items.find(params[:id])
+    render json: item
+  end
+
+  def create
+    item = Item.create(items_params)
+    render json: item, status: :created
+  end
+
+
+  private
+  def items_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response
+    render json: { error: "not found" }, status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
   resources :users, only: [:show]
+
+  get '/users/:user_id/items', to: 'users#index'
+  get '/users/:user_id/items/:id', to: 'users#item'
+  post '/users/:user_id/items', to: 'users#create'
 end
